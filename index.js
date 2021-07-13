7828c0a9607fa64b4170b640c27f28fc67d75f53
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-07-14 01:42:55 +0300

    Fix retrying on network error in Firefox and Safari (#52)

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const retry = require('retry');
 
 const networkErrorMsgs = [
 	'Failed to fetch', // Chrome
-	'NetworkError when attempting to fetch resource', // Firefox
-	'The Internet connection appears to be offline', // Safari
+	'NetworkError when attempting to fetch resource.', // Firefox
+	'The Internet connection appears to be offline.', // Safari
 	'Network request failed' // `cross-fetch`
 ];
 
